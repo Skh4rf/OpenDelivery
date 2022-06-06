d250ed8ae91b9d796ef89cf28feca3d8c3592dbb
Skh4rf <jakob.metzler@gmx.net>
2022-06-07 01:11:35 +0200

    Adding first Map logics

---
+++ src/OpenDelivery/MapPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using OpenDelivery.Services;
+using Windows.Devices.Geolocation;
+using Windows.UI.Xaml.Controls.Maps;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace OpenDelivery
+{
+    public sealed partial class MapPage : Page
+    {
+        private const double DefaultZoomLevel = 15;
+
+        private readonly GPS _gpsService;
+
+        private readonly BasicGeoposition _defaultPosition = new BasicGeoposition() { Latitude = 0, Longitude = 0 };
+
+        public double ZoomLevel { get; set; }
+
+        public Geopoint Center { get; set; }
+
+        public MapPage()
+        {
+            this.InitializeComponent();
+            _gpsService = new GPS();
+            Center = new Geopoint(_defaultPosition);
+            ZoomLevel = DefaultZoomLevel;
+        }
+
+        public async Task InitializeAsync(MapControl map)
+        {
+            if (_gpsService != null)
+            {
+                _gpsService.OnPositionChanged += GpsService_PositionChanged;
+
+                var initializationCheck = await _gpsService.InitializeAsync();
+
+                if (initializationCheck)
+                {
+                    await _gpsService.StartListeningAsnyc();
+                }
+
+                if (initializationCheck && _gpsService.CurrentPosition != null)
+                {
+                    Center = _gpsService.CurrentPosition.Coordinate.Point;
+                }
+                else
+                {
+                    Center = new Geopoint(_defaultPosition);
+                }
+            }
+        }
+
+        private void GpsService_PositionChanged(object sender, Geoposition geoposition)
+        {
+            if (geoposition != null) { Center = geoposition.Coordinate.Point; }
+            mapControl.Center = Center;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await InitializeAsync(mapControl);
+        }
+
+
+    }
+}
---
+++ src/OpenDelivery/Services/GPS.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.ApplicationModel.Core;
+
+namespace OpenDelivery.Services
+{
+    public class GPS
+    {
+        private Geolocator _geolocator;
+
+        public event EventHandler<Geoposition> OnPositionChanged;
+
+        public Geoposition CurrentPosition { get; set; }
+
+        //Aufbau eines Call-Tree zur Ermittlung der Position unter berücksichtigung verschiedener Parameter --> modularität
+        public Task<bool> InitializeAsync() { return InitializeAsync(50); } 
+
+        public Task<bool> InitializeAsync(uint accuracy)
+        {
+            return InitializeAsync(accuracy, (double)accuracy / 2);
+        }
+
+        public async Task<bool> InitializeAsync (uint accuracy, double movementThreshold)
+        {
+            if (_geolocator != null)
+            {
+                _geolocator.PositionChanged -= Geolocator_PositionChanged; //Automatische Standortabfrage deaktivieren
+                _geolocator = null;
+            }
+
+            var access = await Geolocator.RequestAccessAsync();
+
+            bool result;
+
+            switch (access)
+            {
+                case GeolocationAccessStatus.Allowed:
+                    _geolocator = new Geolocator()
+                    {
+                        DesiredAccuracyInMeters = accuracy,
+                        MovementThreshold = movementThreshold
+                    };
+                    result = true;
+                    break;
+                case GeolocationAccessStatus.Unspecified:
+                case GeolocationAccessStatus.Denied:
+                default:
+                    result = false;
+                    break;
+            }
+
+            return result;
+        }
+
+        public async Task StartListeningAsnyc() // Automatische Standortabfrage aktivieren
+        {
+            if (_geolocator == null) { await InitializeAsync(); }
+
+            _geolocator.PositionChanged += Geolocator_PositionChanged;
+
+            CurrentPosition = await _geolocator.GetGeopositionAsync();
+        }
+        
+        public void StopListening() // Automatische Standortabfrage deaktivieren
+        {
+            if (_geolocator == null) { return; }
+
+            _geolocator.PositionChanged -= Geolocator_PositionChanged;
+        }
+
+        private async void Geolocator_PositionChanged(Geolocator sender, PositionChangedEventArgs args) //Erstellen eines Dispatchers zur regelmäßigen Standortüberprüfung
+        {
+            if (args == null) { return; }
+
+            CurrentPosition = args.Position;
+
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                OnPositionChanged?.Invoke(this, CurrentPosition);
+            });
+        }
+    }
+}
