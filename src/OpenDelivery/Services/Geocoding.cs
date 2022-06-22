f11f533d17dfbb1211a968f70c0ab288b1aceaa6
Skh4rf <jakob.metzler@gmx.net>
2022-06-22 09:50:55 +0200

    Adding Geocoding

---
+++ src/OpenDelivery/Services/Geocoding.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.Services.Maps;
+
+namespace OpenDelivery.Services
+{
+    internal class Geocoding
+    {
+        public static async void GeocodeAnAddress(string address, int address_id)
+        {
+            string addressToGeocode = $"{address}";
+
+
+            BasicGeoposition queryHint = new BasicGeoposition();
+            queryHint.Latitude = 47.45020000;  // Alberschwende als Such-Anhaltspunkt
+            queryHint.Longitude = 9.83039000;
+            Geopoint hintPoint = new Geopoint(queryHint);
+
+            MapLocationFinderResult result =
+                await MapLocationFinder.FindLocationsAsync(
+                addressToGeocode,
+                hintPoint,
+                3);
+
+            if (result.Status == MapLocationFinderStatus.Success)
+            {
+                // Koordinaten auf Datenbank laden
+            }
+        }
+    }
+}
